80a9b6f1cadeace76fb9f22cda477eda73112230
hans <h.wellenschritt@tutanota.de>
2022-02-14 14:45:38 +0100

    publish conf

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import typescript from 'rollup-plugin-typescript2'
-import babel from '@rollup/plugin-babel'
 import { terser } from 'rollup-plugin-terser'
 
 export default [
@@ -10,22 +9,6 @@ export default [
       file: 'dist/index.es.js',
       format: 'es'
     },
-    plugins: [typescript(), babel({ extensions: ['.ts'] })]
-  },
-
-  // UMD
-  {
-    input: 'src/index.ts',
-    output: {
-      file: 'dist/index.umd.min.js',
-      format: 'umd',
-      name: 'reactUseThrottle',
-      indent: false
-    },
-    plugins: [
-      typescript(),
-      babel({ extensions: ['.ts'], exclude: 'node_modules/**' }),
-      terser()
-    ]
+    plugins: [typescript(), terser()]
   }
 ]
